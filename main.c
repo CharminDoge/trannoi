6b28ae64e9dc69513d77ed5f6860c4180ee8929e
charmindoge <danielechianella@hotmail.it>
2021-03-25 00:17:27 +0100

    adjusted comments and gioca() if

--- main.c
+++ main.c
@@ -17,11 +17,11 @@ void menu(){
   printf("3) uscire\n");
 }
 
-// la funzione prende una stringa di 128 byte in input
+// la funzione prende una stringa di 32 byte in input
 int getLine(char* inputStr) {
     int chr, check;
 
-    if (fgets (inputStr, 128, stdin) == NULL){
+    if (fgets (inputStr, 32, stdin) == NULL){
       return EMPTY;
     }
     // controllo che almeno 1 carattere sia stato inserito in input
@@ -32,7 +32,7 @@ int getLine(char* inputStr) {
     // controllo a fine linea se trovo \n
     if (inputStr[len - 1] != '\n') {
         check = 0;
-        // 
+        // controllo se la lunghezza della stringa inserita supera la dimensione del buffer
         while (((chr = getchar()) != '\n') && (chr != EOF))
             check = 1;
 
@@ -49,11 +49,12 @@ int getLine(char* inputStr) {
 
 
 int getInt(int* choice){
-  char choiceStr[128];
-  if (getLine(choiceStr) != GOOD)
-    return ERROR;
-  char *end;  //TODO: a che cazzo serve?
-  *choice = strtol(choiceStr, &end, 10);
+  char choiceStr[2];
+  int result = getLine(choiceStr);
+  if ( result != GOOD)
+    return result;
+  char *strInit;
+  *choice = strtol(choiceStr, &strInit, 10);
   return GOOD;
 }
 
@@ -66,6 +67,7 @@ int main(int argc, char** argv){
   }
 
   int choice = 0;
+  int set = 0;
   int exit = 0;
   do{
     menu();
@@ -75,11 +77,13 @@ int main(int argc, char** argv){
       switch (choice) {
         case 1:
           imposta_gioco();
+          set = 1;
           break;
         case 2:
-          printHello();
-          // TODO
-          // non si può giocare se prima non è stato impostato il gioco
+          if (set == 0){
+            printf("Per giocare devi prima impostare il gioco\n");
+            break;
+          }
           gioca();
           break;
         case 3:
